3c0544c1e96eb571ea489c151621e938d234e749
Akos Grabecz <akos.grabecz@sap.com>
2025-07-28 10:14:29 +0200

    high level something

--- SwiftUIViewTree/ContentView.swift
+++ SwiftUIViewTree/ContentView.swift
@@ -9,16 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//            .font(.largeTitle)
+//            .bold()
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if true {
+                Text("Hello, 2!")
+            } else {
+                Text("Hello, 3!")
+            }
         }
-        .padding()
+                .debug()
+//            .bold()
+//        }
+//        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+extension Mirror {
+    func printRecursively() {
+        if children.isEmpty {
+            print("end of branch of \(self)")
+            return
+        }
+        print("Ákos description", self.subjectType)
+        for (idx, child) in children.enumerated() {
+            let label = child.label ?? "<unknown>"
+            let value = child.value
+            print(
+                "|_ Ákos",
+                idx,
+                "\(type(of: value))",
+//                label,
+//                value,
+                separator: " | "
+            )
+
+            Mirror(reflecting: value)
+                .printRecursively()
+        }
+    }
+}
+
+public extension View {
+    func debug() -> some View {
+        print("Ákos")
+        //        print(body)
+        Mirror(reflecting: self).printRecursively()
+        return self
+    }
 }
