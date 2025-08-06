b76cee550b2503005891870199ebd554cb2900d5
Akos Grabecz <akos.grabecz@gmail.com>
2025-08-06 23:48:22 +0200

    ShortenableString to be observable and equatable

--- SwiftUIViewTree/Internal/DataStructures/ShortenableString.swift
+++ SwiftUIViewTree/Internal/DataStructures/ShortenableString.swift
@@ -5,9 +5,12 @@
 //  Created by Grabecz, Akos on 2025. 07. 29..
 //
 
-struct ShortenableString: Equatable {
+import Observation
+
+@Observable
+final class ShortenableString: Equatable {
     let fullString: String
-    var maxLength: Int = 20
+    var maxLength = 20
 
     var shortenedString: String {
         if fullString.count <= maxLength {
@@ -15,5 +18,15 @@ struct ShortenableString: Equatable {
         } else {
             String(fullString.prefix(maxLength)) + "..."
         }
+    }
+
+    init(fullString: String, maxLength: Int = 20) {
+        self.fullString = fullString
+        self.maxLength = maxLength
+    }
+
+    static func == (lhs: ShortenableString, rhs: ShortenableString) -> Bool {
+        lhs.fullString == rhs.fullString &&
+        lhs.maxLength == rhs.maxLength
     }
 }
