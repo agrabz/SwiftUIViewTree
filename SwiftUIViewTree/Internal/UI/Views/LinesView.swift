ddbbf6255a0fa0349ad5a386d6135f30d831efe9
Akos Grabecz <akos.grabecz@gmail.com>
2025-08-13 22:46:47 +0200

    private into private extension

--- SwiftUIViewTree/Internal/UI/Views/LinesView.swift
+++ SwiftUIViewTree/Internal/UI/Views/LinesView.swift
@@ -4,12 +4,32 @@ struct LinesView: View {
     let parentTree: Tree
     let nodeCenters: [String: Anchor<CGPoint>] //TODO: ID collision can happen with this setup so we'd need something else like position in tree or parent ID
 
-    private func pointFor(nodeID: String, in proxy: GeometryProxy) -> CGPoint? { //TODO: to test?
+   var body: some View {
+        GeometryReader { proxy in
+            ForEach(self.parentTree.children, id: \.parentNode.id) { childTree in
+                Group {
+                    self.lineFromParent(
+                        to: childTree,
+                        in: proxy
+                    )?.stroke()
+
+                    LinesView(
+                        parentTree: childTree,
+                        nodeCenters: self.nodeCenters
+                    )
+                }
+            }
+        }
+    }
+}
+
+private extension LinesView {
+    func pointFor(nodeID: String, in proxy: GeometryProxy) -> CGPoint? { //TODO: to test?
         guard let anchor = nodeCenters[nodeID] else { return nil }
         return proxy[anchor]
     }
-    
-    private func lineFromParent(to childTree: Tree, in proxy: GeometryProxy) -> Line? { //TODO: to test?
+
+    func lineFromParent(to childTree: Tree, in proxy: GeometryProxy) -> Line? { //TODO: to test?
         guard
             let startPoint = pointFor(
                 nodeID: self.parentTree.parentNode.id,
@@ -30,22 +50,6 @@ struct LinesView: View {
 
         return lineBetweenStartAndEndPoints
     }
-    
-    var body: some View {
-        GeometryReader { proxy in
-            ForEach(self.parentTree.children, id: \.parentNode.id) { childTree in
-                Group {
-                    self.lineFromParent(
-                        to: childTree,
-                        in: proxy
-                    )?.stroke()
 
-                    LinesView(
-                        parentTree: childTree,
-                        nodeCenters: self.nodeCenters
-                    )
-                }
-            }
-        }
-    }
+
 }
