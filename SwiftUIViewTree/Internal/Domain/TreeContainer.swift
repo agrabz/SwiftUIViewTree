92e3bef542e60e5b92ec76c13305ecf1787de91b
Akos Grabecz <akos.grabecz@gmail.com>
2025-08-13 15:16:03 +0200

    Revert "isReloading approach reloads everything"
    This reverts commit fb972fc120dc53a79b2be4e1ca251a00faae01c0.

--- SwiftUIViewTree/Internal/Domain/TreeContainer.swift
+++ SwiftUIViewTree/Internal/Domain/TreeContainer.swift
@@ -4,16 +4,9 @@ import SwiftUI
 final class TreeContainer {
     static var shared: TreeContainer = .init()
     var tree: Tree?
-    var isReloading = false
 
     func computeViewTree(maxDepth: Int, source: any View) {
         Task {
-            if tree != nil {
-                isReloading = true
-            } else {
-                isReloading = false
-            }
-
             let newTree = Tree(
                 node: .rootNode
             )
@@ -23,14 +16,13 @@ final class TreeContainer {
             )
 
             // Uncomment this to simulate delay in computing the tree
-            try? await Task.sleep(for: .seconds(1))
+//            try? await Task.sleep(for: .seconds(2))
 
             if self.tree != nil {
                 self.tree?.children = newTree.children //replace only what's needed
             } else {
                 self.tree = newTree
             }
-            isReloading = false
         }
     }
 }
