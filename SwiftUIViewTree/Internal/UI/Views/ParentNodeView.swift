c5d5f0589d845566f9fbeb207677642d6e025cf5
Akos Grabecz <akos.grabecz@gmail.com>
2025-08-18 14:03:10 +0200

    clear up

--- SwiftUIViewTree/Internal/UI/Views/ParentNodeView.swift
+++ SwiftUIViewTree/Internal/UI/Views/ParentNodeView.swift
@@ -1,25 +1,17 @@
 import SwiftUI
 
-struct ParentNodeView: View { //TODO: having this layer makes the scrolling and zooming more laggy. maybe because of the popover?
-    @State private var isPopoverPresented = false
+struct ParentNodeView: View {
     @Binding var parentNode: TreeNode
 
     var body: some View {
-//        Button {
-//            isPopoverPresented.toggle()
-//        } label: {
-            NodeView(
-                node: $parentNode
-            )
-            .anchorPreference(key: NodeCenterPreferenceKey.self, value: .center) { anchor in
-                [parentNode.id: anchor]
-            }
-            .onTapGesture {
-                TreeCoordinator.shared.popover = .node(self.parentNode)
-            }
-//        }
-//        .popover(isPresented: $isPopoverPresented) {
-//            NodePopover(node: parentNode)
-//        }
+        NodeView(
+            node: $parentNode
+        )
+        .anchorPreference(key: NodeCenterPreferenceKey.self, value: .center) { anchor in
+            [parentNode.id: anchor]
+        }
+        .onTapGesture {
+            TreeCoordinator.shared.popover = .node(self.parentNode)
+        }
     }
 }
