cd169a6128cc3bfc252ef37d1f0be120ab943916
Akos Grabecz <akos.grabecz@gmail.com>
2025-08-18 14:56:17 +0200

    contextmenu approach

--- SwiftUIViewTree/Internal/UI/Views/ParentNodeView.swift
+++ SwiftUIViewTree/Internal/UI/Views/ParentNodeView.swift
@@ -5,26 +5,22 @@ struct ParentNodeView: View {
     @Binding var parentNode: TreeNode
 
     var body: some View {
-        Menu {
-//            List {
-                Text("Type: `\(parentNode.type)`")   //TODO: to come from node object or from a separate UIModel whose mapper is testable
-                Text("Label: `\(parentNode.label)`")
-                Text("Value: `\(parentNode.value)`")
-                Text("DisplayStyle: `\(parentNode.displayStyle)`")
-                Text("SubjectType: `\(parentNode.subjectType)`")
-                Text("SuperclassMirror: `\(parentNode.superclassMirror)`")
-                Text("mirrorDescription: `\(parentNode.mirrorDescription)`")
-//            }
-
-        } label: {
-            NodeView(
-                node: $parentNode
-            )
-            .anchorPreference(key: NodeCenterPreferenceKey.self, value: .center) { anchor in
-                [parentNode.id: anchor]
-            }
+        NodeView(
+            node: $parentNode
+        )
+        .anchorPreference(key: NodeCenterPreferenceKey.self, value: .center) { anchor in
+            [parentNode.id: anchor]
         }
+        .contextMenu {
+            Text("Type: `\(parentNode.type)`")   //TODO: to come from node object or from a separate UIModel whose mapper is testable
+            Text("Label: `\(parentNode.label)`")
+            Text("Value: `\(parentNode.value)`")
+            Text("DisplayStyle: `\(parentNode.displayStyle)`")
+            Text("SubjectType: `\(parentNode.subjectType)`")
+            Text("SuperclassMirror: `\(parentNode.superclassMirror)`")
+            Text("mirrorDescription: `\(parentNode.mirrorDescription)`")
 
+        }
 //        .onTapGesture {
 //            treeCoordinator.popover = .node(self.parentNode)
 //        }
