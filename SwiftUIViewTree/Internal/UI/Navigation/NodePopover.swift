fcc53f4b2f6c1b836c5d9395235fd746bd85ee64
Akos Grabecz <akos.grabecz@gmail.com>
2025-08-18 14:45:40 +0200

    make it nicer

--- SwiftUIViewTree/Internal/UI/Navigation/NodePopover.swift
+++ SwiftUIViewTree/Internal/UI/Navigation/NodePopover.swift
@@ -4,16 +4,20 @@ struct NodePopover: View {
     let node: TreeNode
 
     var body: some View {
-        List {
-            Text("Type: `\(node.type)`")   //TODO: to come from node object or from a separate UIModel whose mapper is testable
-            Text("Label: `\(node.label)`")
-            Text("Value: `\(node.value)`")
-            Text("DisplayStyle: `\(node.displayStyle)`")
-            Text("SubjectType: `\(node.subjectType)`")
-            Text("SuperclassMirror: `\(node.superclassMirror)`")
-            Text("mirrorDescription: `\(node.mirrorDescription)`")
+        NavigationStack {
+            List {
+                Text("Type: `\(node.type)`")   //TODO: to come from node object or from a separate UIModel whose mapper is testable
+                Text("Label: `\(node.label)`")
+                Text("Value: `\(node.value)`")
+                Text("DisplayStyle: `\(node.displayStyle)`")
+                Text("SubjectType: `\(node.subjectType)`")
+                Text("SuperclassMirror: `\(node.superclassMirror)`")
+                Text("mirrorDescription: `\(node.mirrorDescription)`")
+            }
+            .navigationTitle("Node Details")
+            .navigationBarTitleDisplayMode(.inline)
+            .padding(.all, 8)
+            .presentationDetents([.medium])
         }
-        .padding(.all, 8)
-        .presentationDetents([.medium])
     }
 }
