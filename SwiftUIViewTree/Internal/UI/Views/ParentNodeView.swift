c792b57f153f99297ff9feaef9d1b225d35f7c5b
Akos Grabecz <akos.grabecz@gmail.com>
2025-08-19 12:50:48 +0200

    comment out menu due to its low performance

--- SwiftUIViewTree/Internal/UI/Views/ParentNodeView.swift
+++ SwiftUIViewTree/Internal/UI/Views/ParentNodeView.swift
@@ -5,13 +5,13 @@ struct ParentNodeView: View {
     @Binding var parentNode: TreeNode
 
     var body: some View {
-        Menu {
-            NodeMenuContent(node: parentNode)
-        } label: {
+//        Menu {
+//            NodeMenuContent(node: parentNode)
+//        } label: {
             NodeView(
                 node: $parentNode
             )
-        }
+//        }
         .anchorPreference(key: NodeCenterPreferenceKey.self, value: .center) { anchor in
             [parentNode.id: anchor]
         }
