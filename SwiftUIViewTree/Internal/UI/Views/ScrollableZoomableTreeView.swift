0a38aabec358ac33812b561aa98600e0f191ffa0
Akos Grabecz <akos.grabecz@gmail.com>
2025-08-20 22:00:05 +0200

    no zoom no scroll

--- SwiftUIViewTree/Internal/UI/Views/ScrollableZoomableTreeView.swift
+++ SwiftUIViewTree/Internal/UI/Views/ScrollableZoomableTreeView.swift
@@ -1,9 +1,6 @@
 import SwiftUI
 
 struct ScrollableZoomableTreeView: View {
-    @State private var currentZoom: CGFloat = 0.0
-    @State private var totalZoom: CGFloat = 1.0
-    @State private var scaleAnchor: UnitPoint = .center
     @State var tree: Tree
 
     init(tree: Tree) {
@@ -13,42 +10,28 @@ struct ScrollableZoomableTreeView: View {
     }
 
     var body: some View {
-        ScrollView([.horizontal, .vertical]) {
-            TreeView(tree: $tree)
-                .backgroundPreferenceValue(NodeCenterPreferenceKey.self) { nodeCenters in
-                    LinesView(
-                        parentTree: self.tree,
-                        nodeCenters: nodeCenters
-                    )
-                }
-                .scaleEffect(max(totalZoom + currentZoom, 0.1), anchor: scaleAnchor) // Prevent flipping by clamping scale
-                .background(
-                    LinearGradient(
-                        gradient:
-                            Gradient(
-                                colors: [
-                                    .blue,
-                                    .teal
-                                ]
-                            ),
-                        startPoint: .topLeading,
-                        endPoint: .bottomTrailing
-                    )
+        TreeView(tree: $tree)
+            .backgroundPreferenceValue(NodeCenterPreferenceKey.self) { nodeCenters in
+                LinesView(
+                    parentTree: self.tree,
+                    nodeCenters: nodeCenters
                 )
-                .simultaneousGesture(
-                    MagnifyGesture()
-                        .onChanged { value in
-                            scaleAnchor = value.startAnchor #error("try to use value.startLocation instead, by converting it somehow... or just let it go for now.:)")
-                            currentZoom = value.magnification - 1
-                        }
-                        .onEnded { value in
-                            totalZoom += (value.magnification - 1)
-                            currentZoom = 0
-                        }
+            }
+            .background(
+                LinearGradient(
+                    gradient:
+                        Gradient(
+                            colors: [
+                                .blue,
+                                .teal
+                            ]
+                        ),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
                 )
-        }
-        .onAppear {
-            print("Appear: \(Date())")
-        }
+            )
+            .onAppear {
+                print("Appear: \(Date())")
+            }
     }
 }
