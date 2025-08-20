da93a458bf1929b59800c24b898817c63f23cdb7
Akos Grabecz <akos.grabecz@gmail.com>
2025-08-20 22:29:52 +0200

    Revert "no zoom no scroll"
    This reverts commit 0a38aabec358ac33812b561aa98600e0f191ffa0.

--- SwiftUIViewTree/Internal/UI/Views/ScrollableZoomableTreeView.swift
+++ SwiftUIViewTree/Internal/UI/Views/ScrollableZoomableTreeView.swift
@@ -1,6 +1,9 @@
 import SwiftUI
 
 struct ScrollableZoomableTreeView: View {
+    @State private var currentZoom: CGFloat = 0.0
+    @State private var totalZoom: CGFloat = 1.0
+    @State private var scaleAnchor: UnitPoint = .center
     @State var tree: Tree
 
     init(tree: Tree) {
@@ -10,28 +13,42 @@ struct ScrollableZoomableTreeView: View {
     }
 
     var body: some View {
-        TreeView(tree: $tree)
-            .backgroundPreferenceValue(NodeCenterPreferenceKey.self) { nodeCenters in
-                LinesView(
-                    parentTree: self.tree,
-                    nodeCenters: nodeCenters
+        ScrollView([.horizontal, .vertical]) {
+            TreeView(tree: $tree)
+                .backgroundPreferenceValue(NodeCenterPreferenceKey.self) { nodeCenters in
+                    LinesView(
+                        parentTree: self.tree,
+                        nodeCenters: nodeCenters
+                    )
+                }
+                .scaleEffect(max(totalZoom + currentZoom, 0.1), anchor: scaleAnchor) // Prevent flipping by clamping scale
+                .background(
+                    LinearGradient(
+                        gradient:
+                            Gradient(
+                                colors: [
+                                    .blue,
+                                    .teal
+                                ]
+                            ),
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
                 )
-            }
-            .background(
-                LinearGradient(
-                    gradient:
-                        Gradient(
-                            colors: [
-                                .blue,
-                                .teal
-                            ]
-                        ),
-                    startPoint: .topLeading,
-                    endPoint: .bottomTrailing
+                .simultaneousGesture(
+                    MagnifyGesture()
+                        .onChanged { value in
+                            scaleAnchor = value.startAnchor #error("try to use value.startLocation instead, by converting it somehow... or just let it go for now.:)")
+                            currentZoom = value.magnification - 1
+                        }
+                        .onEnded { value in
+                            totalZoom += (value.magnification - 1)
+                            currentZoom = 0
+                        }
                 )
-            )
-            .onAppear {
-                print("Appear: \(Date())")
-            }
+        }
+        .onAppear {
+            print("Appear: \(Date())")
+        }
     }
 }
