acce6a8468c62c35bff3a074ce122e6df4a76431
Akos Grabecz <akos.grabecz@gmail.com>
2025-08-22 09:51:50 +0200

    clear up comments

--- SwiftUIViewTree/Internal/Domain/TreeContainer.swift
+++ SwiftUIViewTree/Internal/Domain/TreeContainer.swift
@@ -8,63 +8,44 @@ final class TreeContainer {
     private(set) var isRecomputing = false
 
     func computeViewTree(maxDepth: Int, source: any View) {
-        print("---1")
         Task {
             switch uiState {
                 case .computingTree:
-                    print("---2")
                     break
                 case .treeComputed:
-                    print("---3")
                     withAnimation {
-                        print("---4")
                         isRecomputing = true
-                        print("---5")
                     }
             }
 
-            print("---6")
             let newTree = Tree(
                 node: .rootNode
             )
-            print("---7")
             newTree.children = convertToTreesRecursively(
                 mirror: Mirror(reflecting: source),
                 maxDepth: maxDepth
             )
-            print("---8")
 
             // (Un)comment this to simulate delay in computing the tree
             try? await Task.sleep(for: .seconds(1))
-            print("---9")
 
             withAnimation {
-                print("---10")
                 isRecomputing = false
-                print("---11")
             }
 
-            print("---12")
             switch uiState {
                 case .computingTree:
-                    print("---13")
                     withAnimation(.easeInOut(duration: 1)) {
-                        print("---14")
                         self.uiState = .treeComputed(
                             .init(
                                 treeBreakDownOfOriginalContent: newTree
                             )
                         )
-                        print("---15")
                     }
                 case .treeComputed(let computedUIState):
-                    print("---16")
                     computedUIState.treeBreakDownOfOriginalContent.children = newTree.children //once this change is done, the whole view gets recalculated which takes significant time. Caching? Different tree implementation - expandable nodes?
-                    print("---17")
                     withAnimation(.easeInOut(duration: 1)) {
-                        print("---18")
                         self.uiState = .treeComputed(computedUIState) //replace only what's needed, better diffing
-                        print("---19")
                     }
             }
         }
--- SwiftUIViewTree/Internal/UI/Gestures/DragToScrollGesture.swift
+++ SwiftUIViewTree/Internal/UI/Gestures/DragToScrollGesture.swift
@@ -2,13 +2,14 @@ import SwiftUI
 
 struct DragToScrollGesture: Gesture {
     @Binding var offset: CGSize
-    //slow down dragging with the amount of current scale
     let scale: CGFloat
 
-    //probably these values (2000, 1000) won't be good for all use cases
+    //TODO: probably these values (2000, 1000) won't be good for all use cases
+    /// The maximum horizontal offset allowed at the current scale
     private var horizontalMaxScale: CGFloat {
         2000 * ((scale - floor(scale)) + 1)
     }
+    /// The maximum vertical offset allowed at the current scale
     private var verticalMaxScale: CGFloat {
         1000 * ((scale - floor(scale)) + 1)
     }
@@ -31,6 +32,7 @@ struct DragToScrollGesture: Gesture {
 private extension DragToScrollGesture {
     func getNewOffset(from value: DragGesture.Value) -> CGSize {
         var newOffset = self.offset
+        // Scale is taken as a factor to make the dragging feel more natural when zoomed in, i.e. less sensitive.
         newOffset.width += (value.translation.width / 2) / max(self.scale, 1)
         newOffset.height += (value.translation.height / 2) / max(self.scale, 1)
         return newOffset
