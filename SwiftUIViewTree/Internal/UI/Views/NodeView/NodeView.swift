cf28e5df6b2b586b3dceddfe9a75d4a10cc2b456
Akos Grabecz <akos.grabecz@gmail.com>
2025-08-22 10:25:33 +0200

    nodeview's displayed value to be calculated as properties

--- SwiftUIViewTree/Internal/UI/Views/NodeView/NodeView.swift
+++ SwiftUIViewTree/Internal/UI/Views/NodeView/NodeView.swift
@@ -4,18 +4,43 @@ struct NodeView: View {
     @State private var viewModel = NodeViewModel()
     @Binding var node: TreeNode
 
+    private var nodeLabel: String {
+        if node.label.count > 20 {
+            String(node.label.prefix(20)) + "..."
+        } else {
+            node.label
+        }
+    }
+
+    private var nodeType: String {
+        if node.type.count > 20 {
+            String(node.type.prefix(20)) + "..."
+        } else {
+            node.type
+        }
+    }
+
+    private var nodeValue: String {
+        if node.value.count > 20 {
+            String(node.value.prefix(20)) + "..."
+        } else {
+            node.value
+        }
+    }
+
     var body: some View {
         VStack {
-            Text(String(node.label.prefix(20)))
+            Text(self.nodeLabel)
                 .font(.headline)
                 .fontWeight(.black)
 
             HStack {
-                Text(String(node.type.prefix(20)))
+                Text(self.nodeType)
                     .font(.caption)
                     .bold()
-                Text("`\(String(node.value.prefix(20)))`")
+                Text(self.nodeValue)
                     .font(.caption)
+                    .fontDesign(.monospaced)
                     .italic()
             }
             .padding(.all, 8)
@@ -34,7 +59,6 @@ struct NodeView: View {
             RoundedRectangle(cornerRadius: 20)
                 .stroke(.black, lineWidth: 0.5)
         }
-        .frame(width: 200, height: 200)
         .frame(width: 370, height: 200)
         .padding(.all, 8)
     }
