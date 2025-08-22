e7d6b3202fdb710674cced42b6177d1568c97546
Akos Grabecz <akos.grabecz@gmail.com>
2025-08-22 10:09:35 +0200

    remove unnecessary geometryreader

--- SwiftUIViewTree/Internal/Screens/TreeWindow/TreeWindowScreen.swift
+++ SwiftUIViewTree/Internal/Screens/TreeWindow/TreeWindowScreen.swift
@@ -6,39 +6,37 @@ struct TreeWindowScreen<Content: View>: View {
     let originalContent: Content
 
     var body: some View {
-        GeometryReader { geometry in
-            HStack {
-                originalContent
-                    .frame(width: geometry.size.width * UIConstants.ScreenRatioOf.originalContent)
-                    .disabled(TreeContainer.shared.isRecomputing)
-                    .blur(radius: TreeContainer.shared.isRecomputing ? 2.0 : 0.0)
+        HStack {
+            originalContent
+                .frame(width: UIScreen.main.bounds.width * UIConstants.ScreenRatioOf.originalContent)
+                .disabled(TreeContainer.shared.isRecomputing)
+                .blur(radius: TreeContainer.shared.isRecomputing ? 2.0 : 0.0)
 
-                NavigationStack { //TODO: might not be needed anymore, was added originally to support navigation in the tree, namely .popover, but now menu is used. removing this will cause a weird look during the initial computation
-                    switch treeContainer.uiState {
-                        case .computingTree:
-                            ViewTreeTraversalProgressView()
-                                .onDisappear {
-                                    print("ViewTreeTraversalProgressView disappeared at \(Date())")
-                                }
-                        case .treeComputed(let computedUIState):
-                            ZStack {
-                                ScrollableZoomableTreeView(
-                                    tree: computedUIState.treeBreakDownOfOriginalContent
-                                )
-                                .disabled(TreeContainer.shared.isRecomputing)
-                                .blur(radius: TreeContainer.shared.isRecomputing ? 2.0 : 0.0)
+            NavigationStack { //TODO: might not be needed anymore, was added originally to support navigation in the tree, namely .popover, but now menu is used. removing this will cause a weird look during the initial computation
+                switch treeContainer.uiState {
+                    case .computingTree:
+                        ViewTreeTraversalProgressView()
+                            .onDisappear {
+                                print("ViewTreeTraversalProgressView disappeared at \(Date())")
+                            }
+                    case .treeComputed(let computedUIState):
+                        ZStack {
+                            ScrollableZoomableTreeView(
+                                tree: computedUIState.treeBreakDownOfOriginalContent
+                            )
+                            .disabled(TreeContainer.shared.isRecomputing)
+                            .blur(radius: TreeContainer.shared.isRecomputing ? 2.0 : 0.0)
 
-                                if TreeContainer.shared.isRecomputing {
-                                    ViewTreeTraversalProgressView()
-                                }
+                            if TreeContainer.shared.isRecomputing {
+                                ViewTreeTraversalProgressView()
                             }
-                            .onAppear {
-                                print("ScrollableZoomableTreeView appeared from TreeWindowScreen at \(Date())")
-                            }
-                    }
+                        }
+                        .onAppear {
+                            print("ScrollableZoomableTreeView appeared from TreeWindowScreen at \(Date())")
+                        }
                 }
-                .frame(width: geometry.size.width * UIConstants.ScreenRatioOf.viewTree)
             }
+            .frame(width: UIScreen.main.bounds.width * UIConstants.ScreenRatioOf.viewTree)
         }
     }
 }
