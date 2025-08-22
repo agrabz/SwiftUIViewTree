bc6cb3771356a7e9aaa404cb7e9d5d4f3add5c6e
Akos Grabecz <akos.grabecz@gmail.com>
2025-08-22 16:02:54 +0200

    update commented part to be correct too

--- SwiftUIViewTree/DemoApp/ContentView.swift
+++ SwiftUIViewTree/DemoApp/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 //                .font(.largeTitle)
 //                .bold()
 //        }
-//        .renderViewTree() // <--
+//        .renderViewTree(of: self) // <--
 //    }
 //}
 
