a86a3b3e0ce271101daf944c3511e43dcca59b79
Akos Grabecz <80394215+agrabz@users.noreply.github.com>
2025-09-14 21:51:45 +0200

    delete stuff

--- SwiftUIViewTree/Internal/Extensions/Array+Extensions.swift
+++
@@ -1,8 +0,0 @@
-extension Array {
-    func safeGetElement(at index: Int) -> Element? {
-        guard indices.contains(index) else {
-            return nil
-        }
-        return self[index]
-    }
-}
