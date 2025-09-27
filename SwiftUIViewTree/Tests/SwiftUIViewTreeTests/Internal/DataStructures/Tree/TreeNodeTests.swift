b08677afc4b3564677fadda73c3275a03078cd48
Akos Grabecz <akos.grabecz@gmail.com>
2025-09-27 22:27:34 +0200

    basic BackgroundColor tests

--- SwiftUIViewTree/Tests/SwiftUIViewTreeTests/Internal/DataStructures/Tree/TreeNodeTests.swift
+++ SwiftUIViewTree/Tests/SwiftUIViewTreeTests/Internal/DataStructures/Tree/TreeNodeTests.swift
@@ -5,24 +5,78 @@ import Testing
 @MainActor
 @Suite
 struct TreeNodeTests {
-    @Test
-    func isParent_True() async throws {
-        //GIVEN
-        let parent = TreeNode.createMock(childrenCount: 2)
-        //WHEN
-        let isParent = parent.isParent
-        //THEN
-        #expect(isParent == true)
+    @MainActor
+    struct IsParent {
+        @Test
+        func `true`() async throws {
+            //GIVEN
+            let parent = TreeNode.createMock(childrenCount: 2)
+            //WHEN
+            let isParent = parent.isParent
+            //THEN
+            #expect(isParent == true)
+        }
+
+        @Test
+        func `false`() async throws {
+            //GIVEN
+            let parent = TreeNode.createMock(childrenCount: 0)
+            //WHEN
+            let isParent = parent.isParent
+            //THEN
+            #expect(isParent == false)
+        }
     }
 
-    @Test
-    func isParent_False() async throws {
-        //GIVEN
-        let parent = TreeNode.createMock(childrenCount: 0)
-        //WHEN
-        let isParent = parent.isParent
-        //THEN
-        #expect(isParent == false)
+    @MainActor
+    struct BackgroundColor {
+        @Test
+        func noChange() {
+            //GIVEN
+            let node = TreeNode.createMock()
+            //WHEN
+            let result = node.backgroundColor
+            //THEN
+            #expect(result == LinkedColorList().colors.first)
+        }
+
+        @Test
+        func isCollapsed() {
+            //GIVEN
+            let node = TreeNode.createMock()
+            CollapsedNodesStore.shared.toggleCollapse(nodeID: node.id)
+            //WHEN
+            let result = node.backgroundColor
+            //THEN
+            #expect(result == UIConstants.Color.collapsedNodeBackground)
+        }
+
+        @Test
+        func hasChanged() {
+            //GIVEN
+            let node = TreeNode.createMock()
+            node.value = "new value"
+            TreeNodeMemoizer.shared.registerChangedNode(node)
+            //WHEN
+            let result = node.backgroundColor
+            //THEN
+            #expect(result == LinkedColorList().colors.safeGetElement(at: 1))
+            #expect(TreeNodeMemoizer.shared.allChangedNodes.contains(where: { $0.id == node.id }) == false)
+        }
     }
+
+//    var backgroundColor: Color {
+//        if CollapsedNodesStore.shared.isCollapsed(nodeID: id) {
+//            return UIConstants.Color.collapsedNodeBackground
+//        }
+//
+//        guard TreeNodeMemoizer.shared.isNodeChanged(serialNumber: self.serialNumber) else {
+//            return oldBackgroundColor
+//        }
+//
+//        oldBackgroundColor = availableColors.getNextColor()
+//        TreeNodeMemoizer.shared.removeNodeFromAllChangedNodes(serialNumberOfNodeToRemove: serialNumber)
+//        return oldBackgroundColor
+//    }
 }
 
