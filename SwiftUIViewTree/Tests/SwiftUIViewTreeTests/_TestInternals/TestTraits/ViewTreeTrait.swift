9f2e75ac65a81d12247461e596a786b330a9a4df
Akos Grabecz <80394215+agrabz@users.noreply.github.com>
2025-10-26 22:40:54 +0100

    remove isfirst

--- SwiftUIViewTree/Tests/SwiftUIViewTreeTests/_TestInternals/TestTraits/ViewTreeTrait.swift
+++ SwiftUIViewTree/Tests/SwiftUIViewTreeTests/_TestInternals/TestTraits/ViewTreeTrait.swift
@@ -7,18 +7,15 @@ struct ViewTreeTrait: TestTrait, SuiteTrait, TestScoping {
     private let collapsedNodeStore: CollapsedNodesStore
     private let treeNodeRegistry: TreeNodeRegistry
     private let viewTreeLogger: ViewTreeLoggerProtocol
-    private let isFirst: IsFirst
 
     init(
         collapsedNodeStore: @autoclosure () -> CollapsedNodesStore,
         treeNodeRegistry: @autoclosure () -> TreeNodeRegistry,
         viewTreeLogger: @autoclosure () -> ViewTreeLoggerProtocol,
-        isFirst: @autoclosure () -> IsFirst
     ) {
         self.collapsedNodeStore = collapsedNodeStore()
         self.treeNodeRegistry = treeNodeRegistry()
         self.viewTreeLogger = viewTreeLogger()
-        self.isFirst = isFirst()
     }
 
     func provideScope(for test: Test, testCase: Test.Case?, performing function: @Sendable () async throws -> Void) async throws {
@@ -26,9 +23,7 @@ struct ViewTreeTrait: TestTrait, SuiteTrait, TestScoping {
         try await CollapsedNodesStore.$shared.withValue(self.collapsedNodeStore) {
             try await TreeNodeRegistry.$shared.withValue(self.treeNodeRegistry) {
                 try await ViewTreeLogger.$shared.withValue(self.viewTreeLogger) {
-                    try await IsFirst.$shared.withValue(self.isFirst) {
                         try await function()
-                    }
                 }
             }
         }
@@ -40,13 +35,11 @@ extension Trait where Self == ViewTreeTrait {
         collapsedNodesStore: CollapsedNodesStore = .init(),
         treeNodeRegistry: TreeNodeRegistry = .init(),
         viewTreeLogger: ViewTreeLoggerProtocol = ViewTreeLogger(),
-        isFirst: IsFirst = IsFirst()
     ) -> ViewTreeTrait {
         ViewTreeTrait(
             collapsedNodeStore: collapsedNodesStore,
             treeNodeRegistry: treeNodeRegistry,
-            viewTreeLogger: viewTreeLogger,
-            isFirst: isFirst
+            viewTreeLogger: viewTreeLogger
         )
     }
 }
