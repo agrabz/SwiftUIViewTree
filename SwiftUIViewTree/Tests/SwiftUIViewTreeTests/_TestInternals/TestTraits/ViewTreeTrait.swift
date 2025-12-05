9b8ce30cbc6ecea42102c52b46109c70986d52c2
Akos Grabecz <80394215+agrabz@users.noreply.github.com>
2025-12-05 15:40:26 +0100

    add config to testtrait

--- SwiftUIViewTree/Tests/SwiftUIViewTreeTests/_TestInternals/TestTraits/ViewTreeTrait.swift
+++ SwiftUIViewTree/Tests/SwiftUIViewTreeTests/_TestInternals/TestTraits/ViewTreeTrait.swift
@@ -7,15 +7,18 @@ struct ViewTreeTrait: TestTrait, SuiteTrait, TestScoping {
     private let collapsedNodeStore: CollapsedNodesStore
     private let treeNodeRegistry: TreeNodeRegistry
     private let viewTreeLogger: ViewTreeLoggerProtocol
+    private let configuration: Configuration
 
     init(
         collapsedNodeStore: @autoclosure () -> CollapsedNodesStore,
         treeNodeRegistry: @autoclosure () -> TreeNodeRegistry,
         viewTreeLogger: @autoclosure () -> ViewTreeLoggerProtocol,
+        configuration: @autoclosure () -> Configuration,
     ) {
         self.collapsedNodeStore = collapsedNodeStore()
         self.treeNodeRegistry = treeNodeRegistry()
         self.viewTreeLogger = viewTreeLogger()
+        self.configuration = configuration()
     }
 
     func provideScope(for test: Test, testCase: Test.Case?, performing function: @Sendable () async throws -> Void) async throws {
@@ -23,7 +26,9 @@ struct ViewTreeTrait: TestTrait, SuiteTrait, TestScoping {
         try await CollapsedNodesStore.$shared.withValue(self.collapsedNodeStore) {
             try await TreeNodeRegistry.$shared.withValue(self.treeNodeRegistry) {
                 try await ViewTreeLogger.$shared.withValue(self.viewTreeLogger) {
+                    try await Configuration.$shared.withValue(self.configuration) {
                         try await function()
+                    }
                 }
             }
         }
@@ -35,11 +40,13 @@ extension Trait where Self == ViewTreeTrait {
         collapsedNodesStore: CollapsedNodesStore = .init(),
         treeNodeRegistry: TreeNodeRegistry = .init(),
         viewTreeLogger: ViewTreeLoggerProtocol = MockViewTreeLogger(),
+        configuration: Configuration = .init(),
     ) -> ViewTreeTrait {
         ViewTreeTrait(
             collapsedNodeStore: collapsedNodesStore,
             treeNodeRegistry: treeNodeRegistry,
-            viewTreeLogger: viewTreeLogger
+            viewTreeLogger: viewTreeLogger,
+            configuration: configuration
         )
     }
 }
