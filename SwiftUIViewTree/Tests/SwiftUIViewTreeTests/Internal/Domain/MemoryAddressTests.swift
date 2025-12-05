b6b9914e5368d1e6083763e8eb1a01c2add19c83
Akos Grabecz <80394215+agrabz@users.noreply.github.com>
2025-12-05 16:01:42 +0100

    mem tests

--- SwiftUIViewTree/Tests/SwiftUIViewTreeTests/Internal/Domain/MemoryAddressTests.swift
+++ SwiftUIViewTree/Tests/SwiftUIViewTreeTests/Internal/Domain/MemoryAddressTests.swift
@@ -5,7 +5,7 @@ import Testing
 @Suite
 struct MemoryAddressTests {
     @Test
-    func `if neither input is memory address result should be false`() async throws {
+    func `WHEN neither input is memory address THEN result should be false`() async throws {
         //GIVEN
         let string1 = "asd"
         let string2 = "asd"
@@ -15,6 +15,32 @@ struct MemoryAddressTests {
 
         //THEN
         #expect(result == false)
+    }
+
+    @Test
+    func `WHEN both input is memory address AND there is diff THEN result is true`() async throws {
+        //GIVEN
+        let string1 = "0x0123456789aB"
+        let string2 = "0xF9876543210A"
+
+        //WHEN
+        let result = MemoryAddress.hasDiffInMemoryAddress(lhs: string1, rhs: string2)
+
+        //THEN
+        #expect(result == true)
+    }
+
+    @Test
+    func `WHEN both input is memory address AND there is NO diff THEN result is false`() async throws {
+        //GIVEN
+        let string1 = "0x0123456789aB"
+        let string2 = "0x0123456789aB"
+
+        //WHEN
+        let result = MemoryAddress.hasDiffInMemoryAddress(lhs: string1, rhs: string2)
+
+        //THEN
+        #expect(result == true)
     }
 }
 
