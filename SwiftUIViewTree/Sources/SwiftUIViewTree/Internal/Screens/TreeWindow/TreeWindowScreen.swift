1858ea2e90d0f81fdd31f93b51f2fd92307aa3f9
Akos Grabecz <akos.grabecz@gmail.com>
2025-12-09 23:00:28 +0100

    sketchup idae

--- SwiftUIViewTree/Sources/SwiftUIViewTree/Internal/Screens/TreeWindow/TreeWindowScreen.swift
+++ SwiftUIViewTree/Sources/SwiftUIViewTree/Internal/Screens/TreeWindow/TreeWindowScreen.swift
@@ -1,4 +1,51 @@
+
 import SwiftUI
+
+@Observable
+@MainActor
+final class OrientationInfo { //TODO: review concurrency errors
+    enum Orientation {
+        case portrait
+        case landscape
+    }
+
+    static let shared = OrientationInfo()
+
+    var orientation: Orientation
+
+    @ObservationIgnored
+    private var _observer: NSObjectProtocol?
+
+    init() {
+        // fairly arbitrary starting value for 'flat' orientations
+        if UIDevice.current.orientation.isLandscape {
+            self.orientation = .landscape
+        }
+        else {
+            self.orientation = .portrait
+        }
+
+        // unowned self because we unregister before self becomes invalid
+        _observer = NotificationCenter.default
+            .addObserver(forName: UIDevice.orientationDidChangeNotification, object: nil, queue: nil) { [weak self] note in
+            guard let self, let device = note.object as? UIDevice else {
+                return
+            }
+            if device.orientation.isPortrait {
+                self.orientation = .portrait
+            }
+            else if device.orientation.isLandscape {
+                self.orientation = .landscape
+            }
+        }
+    }
+
+    isolated deinit {
+        if let observer = _observer {
+            NotificationCenter.default.removeObserver(observer)
+        }
+    }
+}
 
 struct TreeWindowScreen<Content: View>: View {
     @State private var treeWindowViewModel = TreeWindowViewModel.shared
@@ -9,16 +56,29 @@ struct TreeWindowScreen<Content: View>: View {
     var body: some View {
         GeometryReader { proxy in
             ZStack {
-                HStack {
-                    originalContent
-                        .frame(width: showTree ? (proxy.size.width * UIConstants.ScreenRatioOf.originalContent) : proxy.size.width)
+                if OrientationInfo.shared.orientation == .landscape {
+                    HStack {
+                        originalContent
+                            .frame(width: showTree ? (proxy.size.width * UIConstants.ScreenRatioOf.originalContent) : proxy.size.width)
 
-                    if showTree {
-                        viewFor(uiState: treeWindowViewModel.uiState)
-                            .frame(width: proxy.size.width * UIConstants.ScreenRatioOf.viewTree) //TODO: this should always be the 3/4 of the screen even if we use it on a subview
+                        if showTree {
+                            viewFor(uiState: treeWindowViewModel.uiState)
+                                .frame(width: proxy.size.width * UIConstants.ScreenRatioOf.viewTree) //TODO: this should always be the 3/4 of the screen even if we use it on a subview
+                        }
                     }
+                    .transition(.move(edge: .trailing))
+                } else {
+                    VStack {
+                        originalContent
+                            .frame(height: showTree ? (proxy.size.height * UIConstants.ScreenRatioOf.originalContent) : proxy.size.height)
+
+                        if showTree {
+                            viewFor(uiState: treeWindowViewModel.uiState)
+                                .frame(height: proxy.size.height * UIConstants.ScreenRatioOf.viewTree) //TODO: this should always be the 3/4 of the screen even if we use it on a subview
+                        }
+                    }
+                    .transition(.move(edge: .bottom))
                 }
-                .transition(.move(edge: .trailing))
 
                 ShouldShowTreeButton(shouldShowTree: self.$showTree)
                     .position(
