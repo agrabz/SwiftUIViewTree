1fa3c4b6296aa9f5ee10284ffbeaeb34e25d795e
Akos Grabecz <80394215+agrabz@users.noreply.github.com>
2026-01-04 23:55:12 +0100

    Update ContentView.swift

--- DemoApp/DemoApp/ContentView.swift
+++ DemoApp/DemoApp/ContentView.swift
@@ -13,25 +13,42 @@ struct SubviewToTestWith: View {
         }
 }
 
-struct MyType {
-    let var10: String = "var10"
-    let var11: String = "var11"
-    let var12: String = "var12"
-    let var13: String = "var13"
-    let var14: String = "var14"
-    let var15: String = "var15"
-    let var16: String = "var16"
-    let var17: String = "var17"
-    let var18: String = "var18"
-    let var19: String = "var19"
-    let var20: String = "var20"
-    let var21: String = "var21"
-    let var22: String = "var22"
+struct MyBigType {
+    var var10: String = "var10"
+    var var11: String = "var11"
+    var var12: String = "var12"
+    var var13: String = "var13"
+    var var14: String = "var14"
+    var var15: String = "var15"
+    var var16: String = "var16"
+    var var17: String = "var17"
+    var var18: String = "var18"
+    var var19: String = "var19"
+    var var20: String = "var20"
+    var var21: String = "var21"
+    var var22: String = "var22"
+    let myBigChildType = MyBigChildType()
+}
+
+struct MyBigChildType {
+    let let10: String = "let10"
+    let let11: String = "let11"
+    let let12: String = "let12"
+    let let13: String = "let13"
+    let let14: String = "let14"
+    let let15: String = "let15"
+    let let16: String = "let16"
+    let let17: String = "let17"
+    let let18: String = "let18"
+    let let19: String = "let19"
+    let let20: String = "let20"
+    let let21: String = "let21"
+    let let22: String = "let22"
 }
 
 struct ContentView: View {
     @State private var isTapped = false
-    private let asd = MyType()
+    private let myBigObject = MyBigType()
 
     var body: some View {
         Button {
